5c590a0f84ef58ade7a3762d5ae99a2700b6cafe
Fustigate8933 <ianchang891@gmail.com>
2025-05-13 20:55:17 -0400

    fixed eslint and vue lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,8 +17,6 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			opts.capabilities = capabilities
 
-			local mason_registry = require('mason-registry')
-			local vue_language_server_path = mason_registry.get_package('vue-language-server')
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup(opts)
 			lspconfig.pyright.setup({
@@ -34,10 +32,15 @@ return {
 				},
 			})
 			lspconfig.tailwindcss.setup(opts)
-			lspconfig.eslint.setup(opts)
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+			})
 			lspconfig.clangd.setup(opts)
 
 			-- hybrid mode is enabled by default, where volar handles html/css and ts_ls handels script part
+			local mason_registry = require('mason-registry')
+			local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. "/node_modules/@vue/language-server"
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 				init_options = {
@@ -51,7 +54,6 @@ return {
 				},
 				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 			})
-			lspconfig.volar.setup(opts)
 		end,
 		keys = {
 			{ "K", function() vim.lsp.buf.hover() end, desc = "Display hover information" },
@@ -70,7 +72,7 @@ return {
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
-					require("none-ls.diagnostics.eslint_d"), -- eslint
+					null_ls.builtins.diagnostics.eslint_lsp, -- eslint
 					null_ls.builtins.formatting.stylua, -- stylua is a formatter for lua files
 					null_ls.builtins.formatting.isort, -- formatter for python, sorts imports alphabetically
 					null_ls.builtins.formatting.black, -- general python formatter
